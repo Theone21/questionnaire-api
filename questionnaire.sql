54a1469eaaec4bb2566ba4cf62476240bbd3eccd
jinliujieDELL <jjjjinliujie@163.com>
2020-01-18 22:31:10 +0800

    上传sql文件

--- questionnaire.sql
+++ questionnaire.sql
@@ -10,18 +10,42 @@ Target Server Type    : MYSQL
 Target Server Version : 80018
 File Encoding         : 65001
 
-Date: 2019-12-22 23:37:01
+Date: 2020-01-18 22:30:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sys_function
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_function`;
+CREATE TABLE `sys_function` (
+  `function_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `function_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '权限编码',
+  `function_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '权限名称',
+  `function_type` tinyint(2) DEFAULT NULL COMMENT '权限类型\r\n0表示功能模块；\r\n1表示详细功能；\r\n2表示按钮权限。',
+  `function_parent_id` int(11) DEFAULT NULL COMMENT '所属父功能id',
+  PRIMARY KEY (`function_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8 COMMENT='权限表';
+
+-- ----------------------------
+-- Table structure for sys_roles
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_roles`;
+CREATE TABLE `sys_roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `role_name` varchar(255) DEFAULT NULL COMMENT '角色用户名',
+  `role_code` varchar(255) DEFAULT NULL COMMENT '角色用户编号',
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for sys_users
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_users`;
 CREATE TABLE `sys_users` (
-  `USER_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `USER_NAME` varchar(255) DEFAULT NULL,
-  `USER_PASSWORD` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`USER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户表';
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `user_password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COMMENT='用户表';
